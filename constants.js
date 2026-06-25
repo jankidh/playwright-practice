df1f360baee476bacc0385c1cbfdce2b3c04f15a
OlgaShtante <51260907+OlgaShtante@users.noreply.github.com>
2026-06-25 10:55:51 +0200

    add unsaved constants update

--- constants.js
+++ constants.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 const env = process.env;
 
 export const baseUrl = env.BASE_URL;
+export const inboxUrl = env.INBOX_URL;
 export const userId = env.USER_ID;
 export const userSecretKey = env.USER_SECRET_KEY;
-export const inboxUrl = env.INBOX_URL;
+export const userInboxUrl = `${env.INBOX_URL}${env.USER_ID.split("@")[0]}`;
